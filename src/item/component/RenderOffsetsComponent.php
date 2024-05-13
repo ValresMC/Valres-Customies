b53b9c5d6613f595dca9ef40006016634a081b6a
Valres <tsterle59@gmail.com>
2024-05-13 20:03:02 +0200

    Update RenderOffsetsComponent.php
    The RenderOffsetsComponent it's now better.

--- src/item/component/RenderOffsetsComponent.php
+++ src/item/component/RenderOffsetsComponent.php
@@ -5,38 +5,44 @@ namespace customiesdevs\customies\item\component;
 
 final class RenderOffsetsComponent implements ItemComponent {
 
-	private int $textureWidth;
-	private int $textureHeight;
-	private bool $handEquipped;
+    # Thanks to https://github.com/dadodasyra to the class.
+    private int $textureSize;
 
-	public function __construct(int $textureWidth, int $textureHeight, bool $handEquipped = false) {
-		$this->textureWidth = $textureWidth;
-		$this->textureHeight = $textureHeight;
-		$this->handEquipped = $handEquipped;
-	}
+    public function __construct(int $textureSize) {
+        $this->textureSize = $textureSize;
+    }
 
-	public function getName(): string {
-		return "minecraft:render_offsets";
-	}
+    public function getName(): string {
+        return "minecraft:render_offsets";
+    }
 
-	public function getValue(): array {
-		$horizontal = ($this->handEquipped ? 0.075 : 0.1) / ($this->textureWidth / 16);
-		$vertical = ($this->handEquipped ? 0.125 : 0.1) / ($this->textureHeight / 16);
-		$perspectives = [
-			"first_person" => [
-				"scale" => [$horizontal, $vertical, $horizontal],
-			],
-			"third_person" => [
-				"scale" => [$horizontal, $vertical, $horizontal]
-			]
-		];
-		return [
-			"main_hand" => $perspectives,
-			"off_hand" => $perspectives
-		];
-	}
+    public function getValue(): array {
+        $textureSize = $this->textureSize;
+        $mainHand_fp = round(0.039 * 16 / $textureSize, 8);
+        $offhand_fp = round(0.065 * 16 / $textureSize, 8);
+        $mainHand_tp = $offhand_tp = round(0.0965 * 16 / $textureSize, 8);
 
-	public function isProperty(): bool {
-		return false;
-	}
+        return [
+            "main_hand" => [
+                "first_person" => [
+                    "scale" => [$mainHand_fp, $mainHand_fp, $mainHand_fp],
+                ],
+                "third_person" => [
+                    "scale" => [$mainHand_tp, $mainHand_tp, $mainHand_tp]
+                ]
+            ],
+            "off_hand" => [
+                "first_person" => [
+                    "scale" => [$offhand_fp, $offhand_fp, $offhand_fp],
+                ],
+                "third_person" => [
+                    "scale" => [$offhand_tp, $offhand_tp, $offhand_tp]
+                ]
+            ]
+        ];
+    }
+
+    public function isProperty(): bool {
+        return false;
+    }
 }
